ac7917891db55a70011b707811929b75efe5460d
Tony-Katrib <katribtony4@gmail.com>
2025-05-29 22:16:23 +0300

    Added folder mobile without React Native initialization, added appicons folder inside (to be moved later on intialization)

--- Apps/web/src/pages/Landing.tsx
+++ Apps/web/src/pages/Landing.tsx
@@ -1,6 +1,23 @@
 export default function Landing() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <svg style={{ margin: "30px" }} width="48" height="48" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <path fill="var(--primary-color)" d="
+                    M35,0 H65 V35 H100 V65 H65 V100 H35 V65 H0 V35 H35 Z
+                    M35,35 
+                    L35,10 
+                    A25,25 0 0 1 10,35 
+                    L10,35 
+                    Z
+                    M65,65 
+                    L65,90 
+                    A25,25 0 0 1 90,65 
+                    L90,65 
+                    Z"
+                    fill-rule="evenodd"
+                />
+            </svg>
+
             <h1 className="text-4xl font-bold mb-4">Welcome to Our Healthcare App</h1>
             <p className="text-lg mb-8">Your health, our priority.</p>
             <a
